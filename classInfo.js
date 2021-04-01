56f3bef525f97bfb4c7c4cd3b9e1e6ccf43ee3e1
RiouC <riou.christophe@protonmail.com>
2021-04-01 16:51:42 +0200

    [class] update classInfo.js (export)

--- classInfo.js
+++ classInfo.js
@@ -31,19 +31,5 @@ class Human {
     }
 }
 
-const alice = new Human('Alice', 'Liddell', 28, ['APL', 'J', 'Shakti K', 'R', 'python', 'Julia', 'haskell', 'OCaml', 'erlang', 'Common Lisp', 'Scheme', 'microKanren', 'Shen', 'Prolog', 'Raku', 'Eiffel', 'SmallTalk', 'Pharo', 'coq', 'Emacs Lisp', 'Elm', 'PureScript', 'Factor']);
-const bob = new Human('Bob', 'Lemon', 30, ['rust', 'assembleur', 'C', 'C++', 'Java', 'Brainfuck', 'Befunge', 'Malbolge', 'ArnoldC', 'Cobol', 'Pascal', 'VimScript', 'ruby', 'clojure', 'VHDL', 'Verilog', 'Zig', 'SuperCollider', 'Vlang']);
-const charlie = new Human('Charlie', 'Charlot', 8, ['Scratch', 'thunkable', 'python', 'JavaScool', 'Haxe', 'Lua', 'Neko', 'Tiger', 'Fjölnir', 'LOLCODE', 'PHP', 'Whitespace']);
+exports.Human = Human;
 
-alice.printInfo();
-bob.printInfo();
-charlie.printInfo();
-
-console.log(alice.canVote());
-console.log(bob.canVote());
-console.log(charlie.canVote());
-
-console.log(alice.mostSkilledDev(bob));
-console.log(alice.mostSkilledDev(charlie));
-console.log(bob.mostSkilledDev(charlie));
-console.log(bob.mostSkilledDev(bob));
